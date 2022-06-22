cfea638a8d78bb9c42da78b992f092cbada1d0df
Luccas Rocigno de Medeiros <luccas.medeiros@igs-software.com.br>
2022-06-22 15:05:53 -0300

    new example

--- example_sdl_opengl.c
+++ example_sdl_opengl.c
@@ -1,8 +1,20 @@
 #include <deskpunk_core.h>
 
+SDL_Rect rect;
+
 static void routine_fillRect(void)
 {
-    SDL_FillRect(deskpunk_mainWindow.canvas, NULL, SDL_MapRGB(deskpunk_mainWindow.canvas->format, 0xFF, 0x0, 0x0));
+    int mouseX;
+    int mouseY;
+    uint32_t rect_color = SDL_MapRGB(deskpunk_mainWindow.canvas->format, 0xFF, 0x0, 0x0);
+
+    SDL_GetMouseState(&mouseX, &mouseY);
+
+    if ((rect.x <= mouseX && rect.x >= mouseX - 32) && 
+            (rect.y <= mouseY && rect.y >= mouseY - 32))
+        rect_color = SDL_MapRGB(deskpunk_mainWindow.canvas->format, 0xFF, 0x3A, 0x0);
+
+    SDL_FillRect(deskpunk_mainWindow.canvas, &rect, rect_color);
 }
 
 static int stopCondition_escWasPressed(void)
@@ -25,6 +37,12 @@ static int stopCondition_escWasPressed(void)
 int main(void)
 {
     deskpunk_core_init();
+
+    rect.x = 0;
+    rect.y = 0;
+    rect.w = 32;
+    rect.h = 32;
+
     deskpunk_core_mainLoop(stopCondition_escWasPressed, routine_fillRect);
     deskpunk_core_quit();
 
